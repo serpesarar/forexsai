147c198b29b69a28fdb5c5ab319cf316c4351869
serpesarar <m.canodacioglu@gmail.com>
2026-02-23 20:14:38 -0500

    feat(landing): integrate spline scene for elite forexs ai landing page
    - Added: SplineScene component
    - Changed: Overhauled welcome page to premium 3D design
    - Added: Translations for new landing page keys
    - Test: tsc passes

---
+++ frontend/components/SplineScene.tsx
@@ -0,0 +1,7 @@
+import Spline from '@splinetool/react-spline/next';
+
+export default function SplineScene() {
+    return (
+        <Spline scene="https://prod.spline.design/qwXYdfDuVtIZ2NZZ/scene.splinecode" />
+    );
+}
